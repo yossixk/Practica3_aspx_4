59df845ecd42c046ee97439b9724666035fc286d
Yoss <gironandrea544gmail.com>
2025-03-05 21:27:39 -0600

    Primer commit y tambien push

---
+++ Ejercicio4-aspx/New1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="New1.aspx.cs" Inherits="Ejercicio4_aspx.New1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:label runat="server">Create Category Array</asp:label>
+            <br />
+            <br />
+            <asp:Button runat="server" ID="CreateArray" OnClick="CreateArray_Click" Text="Create Array" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Ejercicio4-aspx/New1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ejercicio4_aspx
+{
+	public partial class New1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btn_Confir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+		protected void CreateArray_Click(object sender, EventArgs e)
+		{
+			//Declare and load the array.
+			String[] strCategoryArray = new String[5];
+			strCategoryArray[0] = "Footwear - Men´s";
+			strCategoryArray[1] = "Clothing - Men´s";
+			strCategoryArray[2] = "Racquets";
+			strCategoryArray[3] = "Footwear - Women´s";
+			strCategoryArray[4] = "Clothing - Women´s";
+			//Save the array in a session variable for future use.
+			Session["strCategoryArray"] = strCategoryArray;
+			// Go to the next page.
+			Response.Redirect("New2.aspx");
+		}
+	}
+}
